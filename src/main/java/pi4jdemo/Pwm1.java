1a020a32e95c6e5529287f0f771c519d49af79e8
ligson <593949938@qq.com>
2016-11-21 21:39:00 +0800

    333

--- src/main/java/pi4jdemo/Pwm1.java
+++ src/main/java/pi4jdemo/Pwm1.java
@@ -3,12 +3,14 @@ package pi4jdemo;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.PinMode;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class Pwm1 {
 	public static void main(String[] args) throws Exception{
 		GpioController gpio = GpioFactory.getInstance();
 		GpioPinPwmOutput pin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_29);
+		pin.setMode(PinMode.PWM_OUTPUT);
 		for(int i = 0;i<100;i++){
 			pin.setPwm(i);
 			Thread.sleep(100);
