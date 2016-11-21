a7c5779cc898c97ebb749858f4ea732b3fda4c83
ligson <593949938@qq.com>
2016-11-21 21:43:55 +0800

    xxx

--- src/main/java/pi4jdemo/Pwm1.java
+++ src/main/java/pi4jdemo/Pwm1.java
@@ -6,12 +6,20 @@ import com.pi4j.io.gpio.GpioPinPwmOutput;
 import com.pi4j.io.gpio.PinMode;
 import com.pi4j.io.gpio.RaspiPin;
 
+/***
+ * // All Raspberry Pi models support a hardware PWM pin on GPIO_01. 69 //
+ * Raspberry Pi models A+, B+, 2B, 3B also support hardware PWM pins: GPIO_23,
+ * GPIO_24, GPIO_26
+ * 
+ * @author ligso
+ *
+ */
 public class Pwm1 {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		GpioController gpio = GpioFactory.getInstance();
-		GpioPinPwmOutput pin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_29);
+		GpioPinPwmOutput pin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_26);
 		pin.setMode(PinMode.PWM_OUTPUT);
-		for(int i = 0;i<100;i++){
+		for (int i = 0; i < 100; i++) {
 			pin.setPwm(i);
 			Thread.sleep(100);
 		}
